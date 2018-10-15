51fc931909a061f845994baf7ab0d4ebf228bd8f
Garrett Yaworski <garrett@Garretts-MBP.2ndFlr-Hallway-21>
2018-10-15 10:07:39 -0500

    rework

---
+++ MyRouter.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Stack, Scene, Router} from 'react-native-router-flux'
+import Home from './components/Home'
+import Content from './components/Content'
+
+const MyRouter = () =>{ 
+  return(
+    <Router>
+      <Stack key='root'>
+        <Scene key='home' component={Home} title='Home'/>
+        <Scene key='content' component={Content} title='Content Screen'/>
+      </Stack>
+    </Router>
+  )
+}
+
+export default MyRouter
+
+
