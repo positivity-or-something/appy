4d8f92b392e4766a0ee69ad91e69e6bf3bde83c0
Rockydonthurtem <charrisq@hotmail.com>
2018-10-15 14:23:45 -0500

    first

---
+++ components/post/Post.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class Post extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <p>Post here!</p>
+      </div>
+    );
+  }
+}
