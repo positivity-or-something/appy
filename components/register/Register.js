f28d38745236b436db624c4e938935ac1ba1f5cb
Garrett Yaworski <garrett@Garretts-MBP.2-A-Rm215>
2018-10-17 17:53:59 -0500

    s3

--- components/register/Register.js
+++ components/register/Register.js
@@ -1,25 +1,19 @@
 import React, { Component } from "react";
-<<<<<<< HEAD
-import {View, TextInput, StyleSheet, Button, Text, TouchableOpacity} from 'react-native'
-=======
-<<<<<<< HEAD
-import { View, TextInput, StyleSheet, Button, Text } from "react-native";
-import { connect } from "react-redux";
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  Button,
+  Text,
+  TouchableOpacity
+} from "react-native";
 import axios from "axios";
 import { setUser } from "../../ducks/reducer";
 import { Actions } from "react-native-router-flux";
-=======
-import {View, TextInput, StyleSheet, Button, Text} from 'react-native'
->>>>>>> master
-import { Actions } from 'react-native-router-flux'
-import { connect } from 'react-redux'
-import axios from 'axios'
-import {setUser} from '../../ducks/reducer'
-import { ImagePicker, Camera, Permissions } from 'expo'
-import { accessKey, secretKey } from '../../keys'
-import { RNS3 } from 'react-native-aws3'
-
->>>>>>> master
+import { connect } from "react-redux";
+import { ImagePicker, Camera, Permissions } from "expo";
+import { accessKey, secretKey } from "../../keys";
+import { RNS3 } from "react-native-aws3";
 
 class Register extends Component {
   constructor() {
@@ -27,48 +21,19 @@ class Register extends Component {
 
     this.state = {
       currentUser: true,
-<<<<<<< HEAD
-      userName: 'Username',
-      passWord: 'Password',
-      firstName: 'First Name',
-      email: 'Email',
-      photoUrl: 'Need Firebase or S3 for this!!',
-      photoName: '',
-      photoType: ''
-    }
-
-    this.imagePermission = this.imagePermission.bind(this)
-  }
-
-  setUser(){
-    const config = {
-      keyPrefix: 's3/',
-      bucket: 'groupprojappy',
-      region: 'us-east-1',
-      accessKey: accessKey,
-      secretKey: secretKey,
-      successActionStatus:201
-    }
-    let file = {
-      uri: this.state.photoUrl,
-      name: this.state.photoName,
-      type: this.state.photoType
-    }
-    this.state.photoUrl !== 'Need Firebase or S3 for this!!' ?
-    RNS3.put(file, config).then(response => console.log(response)) :
-    null
-
-=======
       userName: "Username",
       passWord: "Password",
       firstName: "First Name",
       email: "Email",
-      photoUrl: "Need Firebase or S3 for this!!"
+      photoUrl: "Need Firebase or S3 for this!!",
+      photoName: "",
+      photoType: ""
     };
+
+    this.imagePermission = this.imagePermission.bind(this);
   }
 
   setUser() {
->>>>>>> master
     let body = {
       firstName: this.state.firstName,
       userName: this.state.userName,
@@ -82,66 +47,68 @@ class Register extends Component {
       .catch(err => console.log(err));
   }
 
-<<<<<<< HEAD
-
   async imagePermission() {
     const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
-    if (status === 'granted') {
-      console.log("PERMISSION GRANTED")
+    if (status === "granted") {
+      console.log("PERMISSION GRANTED");
       let result = await ImagePicker.launchImageLibraryAsync({
         allowsEditing: true,
-        aspect: [4, 3],
+        aspect: [4, 3]
       });
-      if(!result.cancelled){
-        this.setState({photoUrl: result.uri, photoName: result.fileName, photoType: 'image/png'})
+      if (!result.cancelled) {
+        const config = {
+          keyPrefix: "s3/",
+          bucket: "groupprojappy",
+          region: "us-east-1",
+          accessKey: accessKey,
+          secretKey: secretKey,
+          successActionStatus: 201
+        };
+        let file = {
+          uri: result.uri,
+          name: result.fileName,
+          type: "image/png"
+        };
+        RNS3.put(file, config).then(response =>
+          this.setState({ photoUrl: response.body.postResponse.location })
+        );
       }
     } else {
-      throw new Error('Permissions not granted');
+      throw new Error("Permissions not granted");
     }
   }
 
-
-=======
->>>>>>> master
   render() {
-    console.log(this.state)
+    console.log(this.state);
     return (
       <View style={styles.container}>
-<<<<<<< HEAD
         <TextInput
+          autoCapitalize="none"
           placeholder={this.state.userName}
           onChangeText={text => this.setState({ userName: text })}
         />
         <TextInput
+          autoCapitalize="none"
           placeholder={this.state.passWord}
           onChangeText={text => this.setState({ passWord: text })}
         />
         {this.state.currentUser ? (
-          <View style="none">
-            <Button title="Login" onPress={() => this.setUser()} />
-=======
-        <TextInput autoCapitalize='none' placeholder={this.state.userName} onChangeText={(text) => this.setState({userName: text})}/>
-        <TextInput autoCapitalize='none' placeholder={this.state.passWord} onChangeText={(text) => this.setState({passWord: text})}/>
-        {
-          this.state.currentUser ?
           <View>
-            <Button title="Login" onPress={() => {
-              this.setUser()
-              Actions.home()
-              }}></Button>
->>>>>>> master
+            <Button
+              title="Login"
+              onPress={() => {
+                this.setUser();
+                Actions.home();
+              }}
+            />
             <Text>First Time Here?</Text>
             <Button
               title="Register"
               onPress={() => this.setState({ currentUser: false })}
             />
-            <Button title="Home" onPress={() => Actions.home()}>
-              Home
-            </Button>
           </View>
-<<<<<<< HEAD
         ) : (
-          <View style="none">
+          <View>
             <TextInput
               placeholder={this.state.firstName}
               onChangeText={text => this.setState({ firstName: text })}
@@ -150,19 +117,16 @@ class Register extends Component {
               placeholder={this.state.email}
               onChangeText={text => this.setState({ email: text })}
             />
-            <TextInput placeholder={this.state.photoUrl} />
-            <Button title="Submit" onPress={() => this.setUser()} />
-=======
-          :
-          <View>
-            <TextInput placeholder={this.state.firstName} onChangeText={(text) => this.setState({firstName: text})}/>
-            <TextInput placeholder={this.state.email} onChangeText={(text) => this.setState({email: text})}/>
-            <TouchableOpacity onPress={this.imagePermission}><Text>Select Image</Text></TouchableOpacity>
-            <Button title="Submit" onPress={() => {
-              this.setUser()
-              Actions.home()}}>
-            </Button>
->>>>>>> master
+            <TouchableOpacity onPress={this.imagePermission}>
+              <Text>Select Image</Text>
+            </TouchableOpacity>
+            <Button
+              title="Submit"
+              onPress={() => {
+                this.setUser();
+                Actions.home();
+              }}
+            />
           </View>
         )}
       </View>
--- server/index.js
+++ server/index.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-require("dotenv").config();
-const express = require("express");
-const { json } = require("body-parser");
-const massive = require("massive");
-const cors = require("cors");
-const session = require("express-session");
-const app = express();
-const { getUsers, getUser } = require("./controllers/userCtrl");
-const { post, getPost, getContent } = require("./controllers/Post_controller");
-=======
 require('dotenv').config()
 const express = require('express')
 const { json } = require('body-parser')
@@ -17,8 +6,7 @@ const cors = require('cors')
 const session = require('express-session')
 const app = express()
 const { getUsers, getUser } = require('./controllers/userCtrl')
-const { post, getPost, upVote, downVote } = require('./controllers/Post_controller')
->>>>>>> master
+const { post, getPost, upVote, downVote, getContent } = require('./controllers/Post_controller')
 
 app.use(json());
 app.use(cors());
@@ -48,13 +36,7 @@ app.get("/api/post/:id", getPost);
 //----------------------Content---------------------------------
 app.get("/api/content", getContent);
 
-<<<<<<< HEAD
-app.listen(process.env.SERVER_PORT, () =>
-  console.log(`listening on port ${process.env.SERVER_PORT}`)
-);
-=======
 app.post('/api/upvote/:id', upVote)
 app.post('/api/downvote/:id', downVote)
 
 app.listen(process.env.SERVER_PORT, () => console.log(`listening on port ${process.env.SERVER_PORT}`))
->>>>>>> master
