0d6c3bcd530118ac9a1919127d1fe5c61f175458
Garrett Yaworski <garrett@Garretts-MBP.2-A-Rm215>
2018-10-18 11:50:04 -0500

    endpoints changed for windows

--- db/add_post.sql
+++ db/add_post.sql
@@ -1,3 +1,3 @@
-INSERT INTO content (user_id, title, body, date, category)
-VALUES($1, $2, $3, $4, $5);
+INSERT INTO content (user_id, title, body, date, category, image)
+VALUES($1, $2, $3, $4, $5, $6);
 SELECT * FROM content
