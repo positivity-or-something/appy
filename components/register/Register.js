2f787e92a94e488e6ddf5ccb8e1e714f8f5a6c66
Garrett Yaworski <garrett@Garretts-MBP.2-A-Rm215>
2018-10-22 14:11:13 -0500

    monday

--- components/register/Register.js
+++ components/register/Register.js
@@ -7,7 +7,7 @@ import {
   Text,
   Platform
 } from "react-native";
-import { Icon } from "react-native-elements";
+import { Icon, CheckBox } from "react-native-elements";
 import axios from "axios";
 import { setUser } from "../../ducks/reducer";
 import { Actions } from "react-native-router-flux";
@@ -26,7 +26,18 @@ class Register extends Component {
       passWord: "Password",
       firstName: "First Name",
       email: "Email",
-      photoUrl: "Need Firebase or S3 for this!!"
+      photoUrl: "Need Firebase or S3 for this!!",
+      interests: '',
+      motivation: false,
+      achievement: false,
+      lifestyle: false,
+      work: false,
+      determination: false,
+      inspiration: false,
+      positiveVibes: false,
+      entrepreneur: false,
+      happy: false,
+      celebration: false
     };
 
     this.imagePermission = this.imagePermission.bind(this);
@@ -38,7 +49,8 @@ class Register extends Component {
       userName: this.state.userName,
       passWord: this.state.passWord,
       email: this.state.email,
-      photoUrl: this.state.photoUrl
+      photoUrl: this.state.photoUrl,
+      interests: this.state.interests
     };
     axios
       .post(
@@ -92,6 +104,7 @@ class Register extends Component {
   }
 
   render() {
+    console.log(this.state)
     return (
       <View style={styles.container}>
         <TextInput
@@ -131,6 +144,126 @@ class Register extends Component {
             />
             <Icon onPress={() => this.imagePermission()} name="camera-alt" />
             <Icon onPress={() => this.imagePermission(true)} name="image" />
+            <CheckBox 
+              center
+              title='Motivation'
+              iconType='material'
+              checkedIcon='check-box'
+              uncheckedIcon='check-box-outline-blank'
+              checked={this.state.motivation}
+              onPress={() => {
+              this.setState({motivation: true});
+              this.state.interests ? 
+              this.setState({interests: `${this.state.interests},Motivation`}) 
+              : this.setState({interests: `Motivation`})}}/>
+              <CheckBox 
+              center
+              title='Achievement'
+              iconType='material'
+              checkedIcon='check-box'
+              uncheckedIcon='check-box-outline-blank'
+              checked={this.state.achievement}
+              onPress={() => {
+                this.setState({achievement: true});
+                this.state.interests ? 
+                this.setState({interests: `${this.state.interests},Achievement`})
+                : this.setState({interests: `Achievement`})}}/>
+              <CheckBox 
+              center
+              title='Lifestyle'
+              iconType='material'
+              checkedIcon='check-box'
+              uncheckedIcon='check-box-outline-blank'
+              checked={this.state.lifestyle}
+              onPress={() => {
+              this.setState({lifestyle: true});
+              this.state.interests ? 
+              this.setState({interests: `${this.state.interests},Lifestyle`}) 
+              : this.setState({interests: `Lifestyle`})}}/>
+              <CheckBox 
+              center
+              title='Work'
+              iconType='material'
+              checkedIcon='check-box'
+              uncheckedIcon='check-box-outline-blank'
+              checked={this.state.work}
+              onPress={() => {
+              this.setState({work: true});
+              this.state.interests ? 
+              this.setState({interests: `${this.state.interests},Work`})
+              : this.setState({interests: `Work`})}}/>
+              <CheckBox 
+              center
+              title='Determination'
+              iconType='material'
+              checkedIcon='check-box'
+              uncheckedIcon='check-box-outline-blank'
+              checked={this.state.determination}
+              onPress={() => {
+              this.setState({determination: true});
+              this.state.interests ? 
+              this.setState({interests: `${this.state.interests},Determination`}) 
+              : this.setState({interests: `Determination`})}}/>
+              <CheckBox 
+              center
+              title='Inspiration'
+              iconType='material'
+              checkedIcon='check-box'
+              uncheckedIcon='check-box-outline-blank'
+              checked={this.state.inspiration}
+              onPress={() => {
+              this.setState({inspiration: true});
+              this.state.interests ? 
+              this.setState({interests: `${this.state.interests},Inspiration`})
+              : this.setState({interests: `Inspiration`})}}/>
+              <CheckBox 
+              center
+              title='PositiveVibes'
+              iconType='material'
+              checkedIcon='check-box'
+              uncheckedIcon='check-box-outline-blank'
+              checked={this.state.positiveVibes}
+              onPress={() => {
+              this.setState({positiveVibes: true});
+              this.state.interests ? 
+              this.setState({interests: `${this.state.interests},PositiveVibes`}) 
+              : this.setState({interests: `PositiveVibes`})}}/>
+              <CheckBox 
+              center
+              title='Entrepreneur'
+              iconType='material'
+              checkedIcon='check-box'
+              uncheckedIcon='check-box-outline-blank'
+              checked={this.state.entrepreneur}
+              onPress={() => {
+              this.setState({entrepreneur: true});
+              this.state.interests ? 
+              this.setState({interests: `${this.state.interests},Entrepreneur`})
+              : this.setState({interests: `Entrepreneur`})}}/>
+               <CheckBox 
+              center
+              title='Happy'
+              iconType='material'
+              checkedIcon='check-box'
+              uncheckedIcon='check-box-outline-blank'
+              checked={this.state.happy}
+              onPress={() => {
+              this.setState({happy: true});
+              this.state.interests ? 
+              this.setState({interests: `${this.state.interests},Happy`}) 
+              : this.setState({interests: `Happy`})}}/>
+              <CheckBox 
+              center
+              title='Celebration'
+              iconType='material'
+              checkedIcon='check-box'
+              uncheckedIcon='check-box-outline-blank'
+              checked={this.state.celebration}
+              onPress={() => {
+              this.setState({celebration: true});
+              this.state.interests ? 
+              this.setState({interests: `${this.state.interests},Celebration`})
+              : this.setState({interests: `Celebration`})}}/>
             <Button
               title="Submit"
               onPress={() => {
--- db/register_user.sql
+++ db/register_user.sql
@@ -1,3 +1,3 @@
-INSERT INTO users(user_name, pass_word, first_name, email, image_url)
-VALUES ($1, $2, $3, $4, $5);
+INSERT INTO users(user_name, pass_word, first_name, email, image_url, interests)
+VALUES ($1, $2, $3, $4, $5, $6);
 SELECT * FROM users WHERE user_name = $1
