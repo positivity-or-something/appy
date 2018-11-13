8fb588a2f629f76b6fb640f1f563b505a348988f
Garrett Yaworski <garrett@Garretts-MBP.attlocal.net>
2018-11-13 13:27:12 -0600

    footer cleanup

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import { Actions } from "react-native-router-flux";
 import { Icon } from "react-native-elements";
 
